748f82c9686e7625a4859912733068307814ebc7
yshl <yshl@takechiyo.net>
2013-05-12 22:25:05 +0900

    optimize dgemm

--- src/myblas3.c
+++ src/myblas3.c
@@ -16,18 +16,24 @@ static void my_dgemm_NN(size_t m, size_t n, size_t k,
     // c[(0:m)*ldc+(0:n)], a[(0:m)*lda+(0:k)] b[(0:k)*ldb+(0:n)]
     size_t i,j,l;
     const size_t blocksize=48;
+    double tmp[blocksize][blocksize];
+
     for(i=0; i<m; i+=blocksize){
 	size_t iend=imin(i+blocksize, m);
 	for(l=0; l<k; l+=blocksize){
 	    size_t lend=imin(l+blocksize, k);
+	    size_t ii,jj,ll;
+	    for(ii=i; ii<iend; ii++){
+		for(ll=l; ll<lend; ll++){
+		    tmp[ii-i][ll-l]=alpha*a[ii*lda+ll];
+		}
+	    }
 	    for(j=0; j<n; j+=blocksize){
 		size_t jend=imin(j+blocksize, n);
-		size_t ii,jj,ll;
 		for(ii=i; ii<iend; ii++){
 		    for(ll=l; ll<lend; ll++){
-			double ail=alpha*a[ii*lda+ll];
 			for(jj=j; jj<jend; jj++){
-			    c[ii*ldc+jj]+=ail*b[ll*ldb+jj];
+			    c[ii*ldc+jj]+=tmp[ii-i][ll-l]*b[ll*ldb+jj];
 			}
 		    }
 		}
@@ -45,18 +51,24 @@ static void my_dgemm_NT(size_t m, size_t n, size_t k,
     // c[(0:m)*ldc+(0:n)], a[(0:m)*lda+(0:k)] b[(0:n)*ldb+(0:k)]
     size_t i,j,l;
     const size_t blocksize=48;
+    double tmp[blocksize][blocksize];
     for(i=0; i<m; i+=blocksize){
 	size_t iend=imin(i+blocksize,m);
-	for(j=0; j<n; j+=blocksize){
-	    size_t jend=imin(j+blocksize,n);
-	    for(l=0; l<k; l+=blocksize){
-		size_t lend=imin(l+blocksize,k);
-		size_t ii,jj,ll;
+	for(l=0; l<k; l+=blocksize){
+	    size_t lend=imin(l+blocksize,k);
+	    size_t ii,jj,ll;
+	    for(ii=i; ii<iend; ii++){
+		for(ll=l; ll<lend; ll++){
+		    tmp[ii-i][ll-l]=a[ii*lda+ll];
+		}
+	    }
+	    for(j=0; j<n; j+=blocksize){
+		size_t jend=imin(j+blocksize,n);
 		for(ii=i; ii<iend; ii++){
 		    for(jj=j; jj<jend; jj++){
 			double sum=0.0;
 			for(ll=l; ll<lend; ll++){
-			    sum+=a[ii*lda+ll]*b[jj*ldb+ll];
+			    sum+=tmp[ii-i][ll-l]*b[jj*ldb+ll];
 			}
 			c[ii*ldc+jj]+=alpha*sum;
 		    }
@@ -74,18 +86,23 @@ static void my_dgemm_TN(size_t m, size_t n, size_t k,
     // c[(0:m)*ldc+(0:n)], a[(0:k)*lda+(0:m)] b[(0:k)*ldb+(0:n)]
     size_t i,j,l;
     const size_t blocksize=48;
+    double tmp[blocksize][blocksize];
     for(l=0; l<k; l+=blocksize){
 	size_t lend=imin(l+blocksize,k);
 	for(i=0; i<m; i+=blocksize){
 	    size_t iend=imin(i+blocksize,m);
+	    size_t ii,jj,ll;
+	    for(ll=l; ll<lend; ll++){
+		for(ii=i; ii<iend; ii++){
+		    tmp[ll-l][ii-i]=alpha*a[ll*lda+ii];
+		}
+	    }
 	    for(j=0; j<n; j+=blocksize){
 		size_t jend=imin(j+blocksize,n);
-		size_t ii,jj,ll;
 		for(ll=l; ll<lend; ll++){
 		    for(ii=i; ii<iend; ii++){
-			double ali=alpha*a[ll*lda+ii];
 			for(jj=j; jj<jend; jj++){
-			    c[ii*ldc+jj]+=ali*b[ll*ldb+jj];
+			    c[ii*ldc+jj]+=tmp[ll-l][ii-i]*b[ll*ldb+jj];
 			}
 		    }
 		}
@@ -102,18 +119,24 @@ static void my_dgemm_TT(size_t m, size_t n, size_t k,
     // c[(0:m)*ldc+(0:n)], a[(0:k)*lda+(0:m)] b[(0:n)*ldb+(0:k)]
     size_t i,j,l;
     const size_t blocksize=48;
-    for(j=0; j<n; j+=blocksize){
-	size_t jend=imin(j+blocksize,n);
-	for(i=0; i<m; i+=blocksize){
-	    size_t iend=imin(i+blocksize,m);
-	    for(l=0; l<k; l+=blocksize){
-		size_t lend=imin(l+blocksize,k);
-		size_t ii,jj,ll;
+    double tmp[blocksize][blocksize];
+    for(i=0; i<m; i+=blocksize){
+	size_t iend=imin(i+blocksize,m);
+	for(l=0; l<k; l+=blocksize){
+	    size_t lend=imin(l+blocksize,k);
+	    size_t ii,jj,ll;
+	    for(ii=i; ii<iend; ii++){
+		for(ll=l; ll<lend; ll++){
+		    tmp[ii-i][ll-l]=a[ll*lda+ii];
+		}
+	    }
+	    for(j=0; j<n; j+=blocksize){
+		size_t jend=imin(j+blocksize,n);
 		for(ii=i; ii<iend; ii++){
 		    for(jj=j; jj<jend; jj++){
 			double sum=0.0;
 			for(ll=l; ll<lend; ll++){
-			    sum+=a[ll*lda+ii]*b[jj*ldb+ll];
+			    sum+=tmp[ii-i][ll-l]*b[jj*ldb+ll];
 			}
 			c[ii*ldc+jj]+=alpha*sum;
 		    }
